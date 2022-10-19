8468163f2d4d41a31d5b2b52c57f9580ee39287a
DenisKrinitsyn <skylinekrin@gmail.com>
2022-10-20 04:07:35 +0500

    Completed task1-5

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Human {
+public class Employee {
+
+    String FIO, post, email, telNumber;
+    int cost, age;
+
+
+    Employee(String FIO) {
+        this(FIO, "Менеджер");
+
+    }
+
+    Employee(String FIO, String post) {
+        this(FIO, post, "ivanov@gmail.com");
+
+    }
+
+    Employee(String FIO, String post, String email) {
+        this(FIO, post, email, "+79________");
+
+    }
+
+    Employee(String FIO, String post, String email, String telNumber) {
+        this(FIO, post, email, telNumber, 50000);
+
+    }
+
+    Employee(String FIO, String post, String email, String telNumber, int cost) {
+        this(FIO, post, email, telNumber, cost, 25);
+    }
+
+    Employee(String FIO, String post, String email, String telNumber, int cost, int age) {
+        this.FIO = FIO;
+        this.post = post;
+        this.email = email;
+        this.telNumber = telNumber;
+        this.cost = cost;
+        this.age = age;
+    }
+
+    public void printEmployee() {
+        if (age > 40) {
+        System.out.printf("ФИО: %s\tДолжность: %s\te-mail: %s\t Номер телефона: %s\tЗарплата: %d\tВозраст: %d\n",
+                FIO, post, email, telNumber, cost, age);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee employee[] = new Employee[5];
+        employee[0] = new Employee("Иванов Иван", "Менеджер", "ivanov@gmail.com", "+79615361220", 35000, 25);
+        employee[1] = new Employee("Дмитриев Андрей", "Инженер", "di@gmail.com", "+7912563768", 50000, 42);
+        employee[2] = new Employee("Васильев Александр", "Технолог", "va@gmail.com", "+79603361510", 45000, 46);
+        employee[3] = new Employee("Романенко павел", "Программист", "rp@gmail.com", "+79361151336", 85000, 35);
+        employee[4] = new Employee("Санников Егор", "Экономист", "se@gmail.com", "+79821331689", 30000, 45);
+
+        for (int i = 0; i < employee.length; i++) {
+            employee[i].printEmployee();
+        }
     }
 }
